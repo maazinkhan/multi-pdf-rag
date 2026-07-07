3abfafe9cea06e66d28076449dd6971f099fc013
Maazin Khan <maazink@vailsys.com>
2026-07-06 21:58:17 -0500

    Added splitter.py using recursivetextsplitter

---
+++ app/splitter.py
@@ -0,0 +1,18 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+
+def split_documents(documents):
+    """
+        Split LangChain Document objects into smaller chunks while
+        preserving metadata.
+    """
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    return chunks
