a9996bf2f5cc8971c79fd2bb8d7b32ce44560c82
Maazin Khan <maazink@vailsys.com>
2026-07-11 13:14:57 -0500

    updated chat.py to have a reusable function

--- app/chat.py
+++ app/chat.py
@@ -3,26 +3,19 @@ from app.vectorstore import load_vectorstore
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-
-# This does not recreate embeddings.It does not read PDFs.
-# It simply says: "Open my existing Chroma database from disk."
 vector_store = load_vectorstore()
 
-#Convert vectorstore into a retriever
-#This creates a higher-level search interface.
 retriever = create_retriever(vector_store)
 
-question = input("Ask a question: ")
+def ask(question):
+    docs = retriever.invoke(question)
 
-docs = retriever.invoke(question)
+    context = "\n\n".join(
+        doc.page_content for doc in docs
+    )
 
-context = "\n\n".join(
-    doc.page_content for doc in docs
-)
-
-
-prompt = ChatPromptTemplate.from_template(
-    """
+    prompt = ChatPromptTemplate.from_template(
+        """
     Answer the question using only the context below.
 
     Context:
@@ -31,19 +24,32 @@ prompt = ChatPromptTemplate.from_template(
     Question:
     {question}
     """
-)
+    )
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash"
-)
 
-messages = prompt.invoke(
-    {
-        "context": context,
-        "question": question
-    }
-)
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash"
+    )
 
-response = llm.invoke(messages)
+    messages = prompt.invoke(
+         {
+             "context": context,
+            "question": question
+         }
+    )
 
-print(response.content)
+    response = llm.invoke(messages)
+
+    return response.content
+
+
+if __name__ == "__main__":
+    while True:
+        question = input("\nAsk a question (or 'quit'): ")
+
+        if question.lower() == "quit":
+            break
+
+        answer = ask(question)
+        print("\nAnswer:")
+        print(answer)
