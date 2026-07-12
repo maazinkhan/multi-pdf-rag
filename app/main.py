5e8d71de28cf22225a7550078595f0469cfee615
Maazin Khan <maazink@vailsys.com>
2026-07-11 19:13:17 -0500

    Integrated FastAPI

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from app.chat import ask
+
+class ChatRequest(BaseModel):
+    question: str
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {
+        "message": "RAG API is running"
+    }
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    answer = ask(question=request.question)
+    return answer
